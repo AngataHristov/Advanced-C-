877665e0b46abba8c698c2ecd9edc0ab12169aa6
AngataHristov <angata.hristov@gmail.com>
2016-02-14 18:26:36 +0200

    fixed

--- StringsAndTextProcessing/LettersChangeNumbers/LettersChangeNumbersMain.cs
+++ StringsAndTextProcessing/LettersChangeNumbers/LettersChangeNumbersMain.cs
@@ -9,7 +9,7 @@
             string input = Console.ReadLine();
 
             string[] strings = input
-                .Split(new[] { ' ','\t' }, StringSplitOptions.RemoveEmptyEntries);
+                .Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
 
             double totalSum = new double();
 
@@ -19,32 +19,34 @@
                 char lastLetter = s[s.Length - 1];
                 uint number = uint.Parse(s.Substring(1, s.Length - 2));
 
-                double firsResult = new double();
+                double result = ProccessFirstLetter(firstLetter, number);
 
-                if (char.IsUpper(firstLetter))
-                {
-                    firsResult = (double)number / (firstLetter - 64);
-                }
-                else
-                {
-                    firsResult = number * (firstLetter - 96);
-                }
-
-                double result = new double();
-
-                if (char.IsUpper(lastLetter))
-                {
-                    result = (double)firsResult - (lastLetter - 64);
-                }
-                else
-                {
-                    result = firsResult + (lastLetter - 96);
-                }
+                result = ProcessLastLetter(lastLetter, result);
 
                 totalSum += result;
             }
 
             Console.WriteLine("{0:F2}", totalSum);
+        }
+
+        private static double ProcessLastLetter(char lastLetter, double firsResult)
+        {
+            if (char.IsUpper(lastLetter))
+            {
+                return (double)firsResult - (lastLetter - 64);
+            }
+
+            return firsResult + (lastLetter - 96);
+        }
+
+        private static double ProccessFirstLetter(char firstLetter, uint number)
+        {
+            if (char.IsUpper(firstLetter))
+            {
+                return (double)number / (firstLetter - 64);
+            }
+
+            return number * (firstLetter - 96);
         }
     }
 }
