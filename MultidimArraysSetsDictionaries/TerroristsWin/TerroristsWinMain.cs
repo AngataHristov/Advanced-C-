0ad75cca472e9e380e4c40049257fcb6e0762118
AngataHristov <angata.hristov@gmail.com>
2016-02-16 17:32:47 +0200

    multidimational arrays

---
+++ MultidimArraysSetsDictionaries/TerroristsWin/TerroristsWinMain.cs
@@ -0,0 +1,72 @@
+﻿namespace TerroristsWin
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class TerroristsWinMain
+    {
+        public static void Main()
+        {
+            string input = Console.ReadLine();
+
+            List<int> boundaries = new List<int>();
+            List<string> bombs = new List<string>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '|')
+                {
+                    boundaries.Add(i);
+                }
+            }
+
+            for (int i = 0; i < boundaries.Count; i += 2)
+            {
+                bombs.Add(input.Substring(boundaries[i] + 1, boundaries[i + 1] - boundaries[i] - 1));
+            }
+
+            List<int> bombsPower = new List<int>();
+
+            for (int i = 0; i < bombs.Count; i++)
+            {
+                int bombLettersSum = 0;
+
+                foreach (var symbol in bombs[i])
+                {
+                    bombLettersSum += symbol;
+                }
+
+                int bombPower = bombLettersSum % 10;
+
+                bombsPower.Add(bombPower);
+            }
+
+            StringBuilder result = new StringBuilder(input);
+            int boundariesIndex = 0;
+
+            for (int i = 0; i < bombs.Count; i++)
+            {
+                int boundariesLength = 2;
+                int explosionRange = 2 * bombsPower[i] + bombs[i].Length + boundariesLength;
+                string textForReplaceing = string.Empty;
+
+                if (boundaries[boundariesIndex] - bombsPower[i] > 0)
+                {
+                    textForReplaceing = input.Substring(boundaries[boundariesIndex] - bombsPower[i], explosionRange);
+                    result.Replace(textForReplaceing, new string('.', explosionRange));
+                }
+                else
+                {
+                    textForReplaceing = input.Substring(0, explosionRange - bombsPower[i]);
+                    result.Replace(textForReplaceing, new string('.', explosionRange - bombsPower[i]));
+                }
+
+                boundariesIndex += 2;
+
+            }
+
+            Console.WriteLine(result.ToString());
+        }
+    }
+}
---
+++ MultidimArraysSetsDictionaries/ToTheStars/ToTheStarsMain.cs
@@ -0,0 +1,68 @@
+﻿namespace ToTheStars
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ToTheStarsMain
+    {
+        public static void Main()
+        {
+            Dictionary<string, Tuple<double, double>> starSystemDictionary = new Dictionary<string, Tuple<double, double>>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                string[] starSystenTokens = Console.ReadLine()
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string starSystemName = starSystenTokens[0];
+                double starSystemCoordinateX = double.Parse(starSystenTokens[1]);
+                double starSystemCoordinateY = double.Parse(starSystenTokens[2]);
+
+                starSystemDictionary[starSystemName] = new Tuple<double, double>(starSystemCoordinateX, starSystemCoordinateY);
+            }
+
+            double[] spaceshipStartCoordinater = Console.ReadLine()
+                    .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                    .Select(double.Parse)
+                    .ToArray();
+
+            int numberOfMoves = int.Parse(Console.ReadLine());
+            double spaceshipX = spaceshipStartCoordinater[0];
+            double spaceshipY = spaceshipStartCoordinater[1];
+
+
+            while (numberOfMoves >= 0)
+            {
+                bool isInStarSystem = false;
+
+                foreach (var pair in starSystemDictionary)
+                {
+                    if (IsInStarSystem(pair.Value.Item1, pair.Value.Item2, spaceshipX, spaceshipY))
+                    {
+                        Console.WriteLine(pair.Key.ToLower());
+
+                        isInStarSystem = true;
+
+                        break;
+                    }
+                }
+
+                if (!isInStarSystem)
+                {
+                    Console.WriteLine("space");
+                }
+
+                spaceshipY++;
+                numberOfMoves--;
+            }
+        }
+
+        private static bool IsInStarSystem(double starX, double starY, double spaceshipX, double spaceshipY)
+        {
+            return ((spaceshipX >= starX - 1 && spaceshipX <= starX + 1) &&
+                    (spaceshipY >= starY - 1 && spaceshipY <= starY + 1));
+        }
+    }
+}
